4575ac420eb3c2d9f6188881c969b8d1e5ce0e37
Navnath Todkari <todkarinavnath2@gmail.com>
2024-10-27 19:52:12 +0530

    new topic extension

---
+++ CodemindBatch13OOPS/ExtensionMethosOld1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodemindBatch13OOPS
+{
+    internal class ExtensionMethosOld1
+    {
+        public int x = 100;
+        public void test1()
+        {
+            Console.WriteLine("Metthod one:" + this.x);
+        }
+        public void test2()
+        {
+            Console.WriteLine("Method two:" + this.x);
+        }
+    }
+}
---
+++ CodemindBatch13OOPS/ExtensionNewMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodemindBatch13OOPS
+{
+    public static class ExtensionNewMethod
+    {
+        public static void Test3(this ExtensionMethosOld1 o)
+        {
+            Console.WriteLine("Method three");
+        }
+    }
+}
